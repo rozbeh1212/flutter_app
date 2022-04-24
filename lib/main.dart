6ea53c580e462ee2c9e1076ed6b11a616714c363
ASUS <eb.mehrdad@gmail.com>
2022-04-24 14:32:20 +0430

    button created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
 
 import 'package:flutter/material.dart';
 
@@ -7,10 +7,36 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void asnwer () {
+    print('Hello World');
+  }
   @override
   Widget build(BuildContext context) {
+    var question = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    
+    ];
     return MaterialApp(
-      home: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: Column(
+          children: [
+            RaisedButton(
+              child:Text('an2'),
+            onPressed: null,
+            ),
+            
+            RaisedButton(
+              child:Text('an2'),
+            onPressed: null,
+            ),
+            
+          ],
+        ),
+      ),
     );
   }
 }
