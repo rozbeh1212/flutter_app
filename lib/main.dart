7937c693ba6bb9f75a76dffd4bc88d4d05dd47d9
ASUS <eb.mehrdad@gmail.com>
2022-04-27 11:15:02 +0430

    imporvemnt

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _MyAppState();
   }
 }
@@ -20,23 +19,39 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
     },
     {
       'questionText': 'Who\'s your favorite instructor?',
-      'answers': ['Max', 'Max', 'Max', 'Max'],
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
     },
   ];
-  var _questionIndex = 0;
 
-  void _answerQuestion() {
+  var _questionIndex = 0;
+  var _totalScore = 0;
+  void _answerQuestion(int score) {
     // var aBool = true;
     // aBool = false;
-
+    _totalScore = _totalScore + score;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -4,11 +4,11 @@ import './question.dart';
 import './answer.dart';
 
 class Quiz extends StatelessWidget {
-  final List<Map<String, Object>> questions;
+  final List<Map<String, Object>> questions; // List of questions 
   final int questionIndex;
-  final Function answerQuestion;
+  final Function answerQuestion; // Function to answer question
 
-  Quiz({
+  Quiz({ // this a constructor to initialize the properties of the class Quiz 
     @required this.questions,
     @required this.answerQuestion,
     @required this.questionIndex,
@@ -19,10 +19,10 @@ class Quiz extends StatelessWidget {
     return Column(
       children: [
         Question(
-          questions[questionIndex]['questionText'],
+          questions[questionIndex]['questionText'] as String
         ),
-        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
-          return Answer(answerQuestion, answer);
+        ...(questions[questionIndex]['answers'] as List<Map<Strin, Object>> ).map((answer) {
+          return Answer(() => answerQuestion(answer['score']), answer['text']);
         }).toList()
       ],
     );
