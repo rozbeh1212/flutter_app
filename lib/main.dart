1440adfd77e6fd9483aaf19801dc4e6c8adcd550
ASUS <eb.mehrdad@gmail.com>
2022-04-28 10:11:33 +0430

    reset functin created

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,14 @@ class _MyAppState extends State<MyApp> {
 
   var _questionIndex = 0;
   var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     // var aBool = true;
     // aBool = false;
